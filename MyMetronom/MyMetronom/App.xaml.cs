a4a5576034c0f315771906768a037cf54e422fab
Jincheol, SEO <jcseo1028@gmail.com>
2025-09-04 11:00:29 +0900

    MyMetronom 신규 프로젝트 추가

---
+++ MyMetronom/MyMetronom/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyMetronom
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
